ea9c18d591277e03647e60ead586d1677af276fe
mborbely <mate.borbely@doctusoft.com>
2015-03-27 14:28:45 +0100

    Added immediate support for JsfCommandLink.

--- jsf-builder/src/main/java/com/doctusoft/jsf/comp/JsfCommandLink.java
+++ jsf-builder/src/main/java/com/doctusoft/jsf/comp/JsfCommandLink.java
@@ -10,24 +10,29 @@ public class JsfCommandLink extends JsfBaseComponent<JsfCommandLink, JsfCommandL
 	public JsfCommandLink(String id) {
 		super(id, new JsfCommandLinkModel());
 	}
-	
+
 	public JsfCommandLink(String id, String caption) {
 		this(id);
 		withCaption(caption);
 	}
-	
+
 	public JsfCommandLink bindCaption(ValueBinding<String> caption) {
 		model.setCaption(caption);
 		return this;
 	}
-	
+
 	public JsfCommandLink withCaption(String caption) {
 		model.setCaption(new ConstantValueBinding<String>(caption));
 		return this;
 	}
-	
+
 	public JsfCommandLink action(EmptyEventHandler handler) {
 		model.setAction(handler);
+		return this;
+	}
+
+	public JsfCommandLink immediate() {
+		model.setImmediate(new ConstantValueBinding<Boolean>(true));
 		return this;
 	}
 }
